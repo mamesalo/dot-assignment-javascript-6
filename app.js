9444efa1fc2318bf1ced0df557f135fc1b3a1df1
mamesalo <117835018+mamesalo@users.noreply.github.com>
2024-10-07 09:28:44 +0300

    Create app.js

---
+++ app.js
@@ -0,0 +1,21 @@
+// Traditional Function:
+function add (a,b){
+  return a + b
+}
+function multiply (a,b){
+  return a * b
+}
+function greet (name){
+  return 'hello, ' + name;
+}
+
+// Arrow Function:
+const  add =  (a,b) =>{
+  return a + b
+}
+const multiply =  (a,b) =>{
+  return a * b
+}
+const greet = (name) => {
+  return 'hello, ' + name;
+}
